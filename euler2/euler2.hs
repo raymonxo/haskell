f8909590c78207e796823d454797f457c5da5ef8
Ray Mitchell <raymonxo@gmail.com>
2013-04-07 22:17:36 -0700

    Added comment explaining what "fibonaccis" definition is.

--- euler2/euler2.hs
+++ euler2/euler2.hs
@@ -2,6 +2,7 @@
 -- Project Euler, Problem 2 (http://projecteuler.net/problem=2)
 -- Answer:  4613732
 --------------------------------------------------------------------------------
+-- List of all Fibonacci numbers
 fibonaccis :: [Integer]
 fibonaccis = 1 : 1 : fibonaccis' 1 1
 	where fibonaccis' n1 n2 = (n1 + n2) : fibonaccis' (n1 + n2) n1
