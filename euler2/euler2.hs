cee1132e784411ad7d1b89c09f6a1823742a8a9f
Ray Mitchell <raymonxo@gmail.com>
2013-04-08 11:29:20 -0700

    Changed function application to use composition and $.

--- euler2/euler2.hs
+++ euler2/euler2.hs
@@ -7,4 +7,4 @@ fibonaccis :: [Integer]
 fibonaccis = 1 : 1 : fibonaccis' 1 1
 	where fibonaccis' n1 n2 = (n1 + n2) : fibonaccis' (n1 + n2) n1
 
-euler2 = sum (filter even (takeWhile (< 4000000) fibonaccis))
+euler2 = sum . filter even $ takeWhile (< 4000000) fibonaccis
