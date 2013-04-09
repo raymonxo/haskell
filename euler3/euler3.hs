c32ba3d12f4e8372e99944d57b27c86cf9b1e126
Ray Mitchell <raymonxo@gmail.com>
2013-04-08 20:18:17 -0700

    Rearranged code.

--- euler3/euler3.hs
+++ euler3/euler3.hs
@@ -2,17 +2,19 @@
 -- Project Euler, Problem 3 (http://projecteuler.net/problem=3)
 -- Answer:  6857
 --------------------------------------------------------------------------------
--- List of all prime factors of n
-primeFactors :: Integer -> [Integer]
-primeFactors n =
-	let
-		factors n' x
-			| x > limit =		[n']
-			| n' `mod` x == 0 =	x : factors (n' `div` x) x
-			| otherwise =		factors n' (x + 1)
-			where
-				limit = floor . sqrt $ fromIntegral n'
-	in
-		factors n 2
+-- Returns list of prime factors of n
+factors :: Integer -> [Integer]
+factors 0 = []
+factors 1 = []
+factors n = factors' n 2
 
-euler3 = maximum $ primeFactors 600851475143
+-- Helper that computes prime factors of n starting from x
+factors' :: Integer -> Integer -> [Integer]
+factors' n x
+	| x > limit =		[n]
+	| n `mod` x == 0 =	x : factors' (n `div` x) x
+	| otherwise =		factors' n (x + 1)
+	where
+		limit = floor . sqrt $ fromIntegral n
+
+euler3 = maximum $ factors 600851475143
