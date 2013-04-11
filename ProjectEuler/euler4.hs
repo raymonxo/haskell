db8fb60010b9d3e71556b14e9766e00ad1b05361
Ray Mitchell <raymonxo@gmail.com>
2013-04-11 15:08:47 -0700

    Refactored project euler problem 4.

--- ProjectEuler/euler4.hs
+++ ProjectEuler/euler4.hs
@@ -4,6 +4,7 @@
 --------------------------------------------------------------------------------
 -- Return whether argument is a palindrome
 isPalindrome :: (Show a) => a -> Bool
-isPalindrome x = shown == reverse shown where shown = show x
+isPalindrome x = s == reverse s
+    where s = show x
 
 euler4 = maximum [x * y | x <- [100..999], y <- [100..x], isPalindrome $ x * y]
