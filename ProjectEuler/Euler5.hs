724cc045bb3cee270b43a3c628828b64c9db34fe
Ray Mitchell <raymonxo@gmail.com>
2013-04-11 18:56:20 -0700

    Solved euler 5

---
+++ ProjectEuler/Euler5.hs
@@ -0,0 +1,28 @@
+--------------------------------------------------------------------------------
+-- Project Euler, Problem 5 (http://projecteuler.net/problem=5)
+-- Answer:  232792560
+--------------------------------------------------------------------------------
+import Data.List
+import EulerCommon
+
+-- My first solution applies the lowest-common-denominator algorithm to a list
+-- of numbers.  Turns out this solution should have been broken into two
+-- functions - one that computes the lowest-common-denominator (same thing
+-- as the least-common-multiple) for two numbers and another that successively
+-- applies the first function to the numbers in the list.  Only after solving
+-- this with my first solution did I realize that the least-common-multiple
+-- function is provided in Haskell (called "lcm").  After realizing this
+-- I implemented solution #2 (much better).
+
+-- Solution #1
+-- Compute prime factors of 1..20 then multiply the greatest power of each
+-- factor.  The smaller powers of each factor are included in the larger powers 
+-- (e.g. 4 = 2^2 is included in 8 = 2^3).
+euler5 = product $
+         foldr (\xs@(x:_) z -> if x `elem` z then z else xs ++ z)
+                []
+                (sort $ concat $ map (group . factors) [1..20])
+
+-- Solution #2
+-- The "better way".
+euler5' = foldl lcm 1 [1..20]
--- ProjectEuler/EulerCommon.hs
+++ ProjectEuler/EulerCommon.hs
@@ -8,18 +8,19 @@ module EulerCommon
 -- List of prime factors of n
 factors :: Integer -> [Integer]
 factors n = factors' n 2
-  where
-    factors' n' x
-        | x > limit =       [n']
-        | n' `mod` x == 0 = x : factors' (n' `div` x) x
-        | otherwise =       factors' n' (x + 1)
-      where
-        limit = floor . sqrt $ fromIntegral n'
+    where
+        factors' n' x
+            | x > limit =       [n']
+            | n' `mod` x == 0 = x : factors' (n' `div` x) x
+            | otherwise =       factors' n' (x + 1)
+            where
+                limit = floor . sqrt $ fromIntegral n'
 
 -- List of all Fibonacci numbers
 fibonaccis :: [Integer]
 fibonaccis = 1 : 1 : fibonaccis' 1 1
-  where fibonaccis' n1 n2 = (n1 + n2) : fibonaccis' (n1 + n2) n1
+    where
+        fibonaccis' n1 n2 = (n1 + n2) : fibonaccis' (n1 + n2) n1
 
 -- Is argument a palindrome?
 isPalindrome :: (Show a) => a -> Bool
