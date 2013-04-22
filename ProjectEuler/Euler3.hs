6fd05f910e49911a3aca9c1175caad733e5a0822
Ray Mitchell <raymonxo@gmail.com>
2013-04-22 16:29:40 -0700

    Refactored euler solutions to have original problem statement.

--- ProjectEuler/Euler3.hs
+++ ProjectEuler/Euler3.hs
@@ -1,5 +1,10 @@
 --------------------------------------------------------------------------------
 -- Project Euler, Problem 3 (http://projecteuler.net/problem=3)
+--
+-- The prime factors of 13195 are 5, 7, 13 and 29.
+--
+-- What is the largest prime factor of the number 600851475143 ?
+--
 -- Answer:  6857
 --------------------------------------------------------------------------------
 import EulerCommon
--- ProjectEuler/Euler5.hs
+++ ProjectEuler/Euler5.hs
@@ -1,5 +1,12 @@
 --------------------------------------------------------------------------------
 -- Project Euler, Problem 5 (http://projecteuler.net/problem=5)
+--
+-- 2520 is the smallest number that can be divided by each of the numbers from
+-- 1 to 10 without any remainder.
+--
+-- What is the smallest positive number that is evenly divisible by all of the
+-- numbers from 1 to 20?
+--
 -- Answer:  232792560
 --------------------------------------------------------------------------------
 import Data.List
