d348d528ebe7fea25be45d7f5221dc085c147e99
Ray Mitchell <raymonxo@gmail.com>
2013-05-01 13:31:53 -0700

    Refactored - removed whitespace around : operator.

--- ProjectEuler/EulerCommon.hs
+++ ProjectEuler/EulerCommon.hs
@@ -9,8 +9,8 @@ module EulerCommon
 
 -- List of all Fibonacci numbers
 fibonaccis :: [Integer]
-fibonaccis = 1 : 1 : fibonaccis' 1 1
-    where fibonaccis' n1 n2 = (n1 + n2) : fibonaccis' (n1 + n2) n1
+fibonaccis = 1:1:fibonaccis' 1 1
+    where fibonaccis' n1 n2 = (n1 + n2):fibonaccis' (n1 + n2) n1
 
 -- Is argument a palindrome?
 isPalindrome :: (Show a) => a -> Bool
@@ -25,14 +25,14 @@ primeFactors :: Integer -> [Integer]
 primeFactors n = primeFactors' n 2
     where primeFactors' n' x
             | x > limit       = [n']
-            | n' `mod` x == 0 = x : primeFactors' (n' `div` x) x
+            | n' `mod` x == 0 = x:primeFactors' (n' `div` x) x
             | otherwise       = primeFactors' n' (x + 1)
             where limit = floor . sqrt $ fromIntegral n'
 
 -- List of all prime numbers
 primes :: [Integer]
-primes = 2 : primes' [2] 3
+primes = 2:primes' [2] 3
     where primes' ps x
             | any (\p -> x `mod` p == 0) ps'= primes' ps (x + 2)
-            | otherwise                     = x : primes' (ps ++ [x]) (x + 2)
+            | otherwise                     = x:primes' (ps ++ [x]) (x + 2)
             where ps' = takeWhile (<= (floor . sqrt $ fromIntegral x)) ps
