73fb286f62b7f057db4774ca22849e5bba1ba0a8
rmitchell <rmitchell@fairwaytech.com>
2013-10-07 12:24:25 -0700

    Solved problems 7 & 8.

---
+++ S99/Problem7.hs
@@ -0,0 +1,19 @@
+--------------------------------------------------------------------------------
+-- S-99 (http://aperiodic.net/phil/scala/s-99/), Problem 7
+--
+-- P07 (**) Flatten a nested list structure.
+--     Example:
+-- 
+--     scala> flatten(List(List(1, 1), 2, List(3, List(5, 8))))
+--     res0: List[Any] = List(1, 1, 2, 3, 5, 8)
+--------------------------------------------------------------------------------
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem a)      = [a]
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+flatten (List [])     = []
+
+-- Example usage:
+-- flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
+-- [1,2,3,4,5]
---
+++ S99/Problem8.hs
@@ -0,0 +1,15 @@
+--------------------------------------------------------------------------------
+-- S-99 (http://aperiodic.net/phil/scala/s-99/), Problem 8
+--
+-- P08 (**) Eliminate consecutive duplicates of list elements.
+--     If a list contains repeated elements they should be replaced with a
+--     single copy of the element. The order of the elements should not be
+--     changed.
+-- 
+--     Example:
+-- 
+--     scala> compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e,
+--                          'e, 'e))
+--     res0: List[Symbol] = List('a, 'b, 'c, 'a, 'd, 'e)
+--------------------------------------------------------------------------------
+compress xs = foldr (\x z -> if z == [] || x /= head z then x:z else z) [] xs
