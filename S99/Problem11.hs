ba7985e6c45ab6b22360ebd93f266bba5eb7fff2
Ray Mitchell <raymonxo@gmail.com>
2014-08-21 05:38:34 -0700

    Code formatting

--- S99/Problem11.hs
+++ S99/Problem11.hs
@@ -16,10 +16,11 @@ data SingleOrMultiple a = Single a | Multiple [a] deriving (Show)
 
 encodeModified :: (Eq a) => [a] -> [SingleOrMultiple a]
 encodeModified = 
-  foldr (\x zs -> case zs of
-      (Single s) : zs'          | x == s -> Multiple [x, s] : zs'
-      (Multiple (m : ms)) : zs' | x == m -> Multiple (x : m : ms) : zs'
-      otherwise                          -> Single x : zs
+  foldr (\x zs ->
+      case zs of
+        (Single s) : zs'          | x == s -> Multiple [x, s] : zs'
+        (Multiple (m : ms)) : zs' | x == m -> Multiple (x : m : ms) : zs'
+        otherwise                          -> Single x : zs
     )
     []
 
