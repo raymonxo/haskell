86fa991c68bd8271fd43e3dc296b8284915abcfd
Ray Mitchell <raymonxo@gmail.com>
2014-08-27 16:49:23 -0700

    Solved p15

---
+++ scala99/p15.hs
@@ -0,0 +1,9 @@
+--------------------------------------------------------------------------------
+-- P15 (**) Duplicate the elements of a list a given number of times.
+-- Example:
+-- scala> duplicateN(3, List('a, 'b, 'c, 'c, 'd))
+-- res0: List[Symbol] = List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd,
+--                           'd, 'd)
+--------------------------------------------------------------------------------
+duplicateN :: Int -> [a] -> [a]
+duplicateN n = concat . map (replicate n)
