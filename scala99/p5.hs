22bde2e54c9da79ce8b36f722cb3a116dffe1821
Ray Mitchell <raymonxo@gmail.com>
2014-08-28 00:22:40 -0700

    Refactored p5

--- scala99/p5.hs
+++ scala99/p5.hs
@@ -7,4 +7,7 @@
 --     scala> reverse(List(1, 1, 2, 3, 5, 8))
 --     res0: List[Int] = List(8, 5, 3, 2, 1, 1)
 --------------------------------------------------------------------------------
-reverse' xs = foldl (flip (:)) [] xs
+import Data.List (foldl')
+
+reverse' :: [a] -> [a]
+reverse' = foldl' (flip (:)) []
