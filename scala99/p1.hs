e16c286473db3badcfe458bb67c6f0645564306f
Ray Mitchell <raymonxo@gmail.com>
2014-08-28 00:49:13 -0700

    Refactored p1

--- scala99/p1.hs
+++ scala99/p1.hs
@@ -7,5 +7,7 @@
 --     scala> last(List(1, 1, 2, 3, 5, 8))
 --     res0: Int = 8
 --------------------------------------------------------------------------------
-last' (x:[]) = x
+last' :: [a] -> Maybe a
+last' []     = Nothing
+last' (x:[]) = Just x
 last' (_:xs) = last' xs
