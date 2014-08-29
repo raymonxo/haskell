aba92cbc495513cb1b75e6e8cd1c301c561c3505
Ray Mitchell <raymonxo@gmail.com>
2014-08-29 15:20:53 -0700

    Solved p17

---
+++ scala99/p17.hs
@@ -0,0 +1,11 @@
+--------------------------------------------------------------------------------
+-- P17 (*) Split a list into two parts.
+-- The length of the first part is given. Use a Tuple for your result.
+-- Example:
+-- 
+-- scala> split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+-- res0: (List[Symbol], List[Symbol]) = (List('a, 'b, 'c),List('d, 'e, 'f, 'g,
+--        'h, 'i, 'j, 'k))
+--------------------------------------------------------------------------------
+split' :: Int -> [a] -> ([a],[a])
+split' n xs = (take n xs, drop n xs)
