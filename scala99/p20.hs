e5988ff626814132f2dfcf9dd79c360b54606827
Ray Mitchell <raymonxo@gmail.com>
2014-08-29 16:34:01 -0700

    Updated p20 to support returning the removed item

--- scala99/p20.hs
+++ scala99/p20.hs
@@ -8,7 +8,8 @@
 -- scala> removeAt(1, List('a, 'b, 'c, 'd))
 -- res0: (List[Symbol], Symbol) = (List('a, 'c, 'd),'b)
 --------------------------------------------------------------------------------
-removeAt :: Int -> [a] -> [a]
-removeAt n xs | n < 0     = xs
-              | otherwise = case splitAt n xs of (ys, [])   -> ys
-                                                 (ys, _:zs) -> ys ++ zs
+removeAt :: Int -> [a] -> ([a], Maybe a)
+removeAt n xs | n < 0     = (xs, Nothing)
+              | otherwise = case splitAt n xs of
+                              (ys, [])   -> (ys, Nothing)
+                              (ys, z:zs) -> (ys ++ zs, Just z)
