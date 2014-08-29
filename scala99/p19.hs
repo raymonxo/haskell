c99517f37b52fd99ab20654a9a057ef5ee6d85f1
Ray Mitchell <raymonxo@gmail.com>
2014-08-29 16:02:34 -0700

    Solved p19

---
+++ scala99/p19.hs
@@ -0,0 +1,15 @@
+--------------------------------------------------------------------------------
+-- P19 (**) Rotate a list N places to the left.
+-- Examples:
+-- scala> rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+-- res0: List[Symbol] = List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c)
+-- 
+-- scala> rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+-- res1: List[Symbol] = List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i)
+--------------------------------------------------------------------------------
+rotate' :: Int -> [a] -> [a]
+rotate' n xs = drop n' xs ++ take n' xs
+               where n'  = if n < 0
+                           then len - ((abs n) `rem` len)
+                           else n `rem` len
+                     len = length xs
