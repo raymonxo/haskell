67b5275fed14050b74268907db313a37d7bcb7bb
rmitchell <rmitchell@fairwaytech.com>
2014-09-02 14:12:16 -0700

    Added comment to p22

--- scala99/p22.hs
+++ scala99/p22.hs
@@ -6,5 +6,10 @@
 --------------------------------------------------------------------------------
 import Data.List (unfoldr)
 
+-- The unfoldr function is a `dual' to foldr: while foldr reduces a list to a
+-- summary value, unfoldr builds a list from a seed value. The function takes
+-- the element and returns Nothing if it is done producing the list or returns
+-- Just (a,b), in which case, a is a prepended to the list and b is used as the 
+-- next element in a recursive call.
 range' :: Int -> Int -> [Int]
 range' i j = unfoldr (\x -> if x > j then Nothing else Just (x, x + 1)) i
