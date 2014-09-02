7ea022b207a873e6e8ac8f0eb040a1de4bf83533
rmitchell <rmitchell@fairwaytech.com>
2014-09-02 14:10:39 -0700

    Refactored p22

--- scala99/p22.hs
+++ scala99/p22.hs
@@ -4,6 +4,7 @@
 -- scala> range(4, 9)
 -- res0: List[Int] = List(4, 5, 6, 7, 8, 9)
 --------------------------------------------------------------------------------
+import Data.List (unfoldr)
+
 range' :: Int -> Int -> [Int]
-range' i j | i <= j    = i : (range' i + 1 j)
-           | otherwise = []
+range' i j = unfoldr (\x -> if x > j then Nothing else Just (x, x + 1)) i
